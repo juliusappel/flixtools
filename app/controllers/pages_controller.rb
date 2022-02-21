4b2d5ac6fcb285bb03afe5e106dc961524063be1
Miguel Lutz <miguellutz31@gmail.com>
2022-02-21 17:21:03 +0100

    Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def show_own_bookings
+    @bookings = Booking.where("user_id = '#{current_user.id}'")
+  end
+
+  def active_listings
+    @appliances = Appliance.where("user_id = '#{current_user.id}'")
+  end
+
+  private
+
+  def user
+    @user = current_user
+  end
+
 end
