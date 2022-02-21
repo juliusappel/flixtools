a58a6083e7d6da287151300df114f38ae541ca2d
Julius Appel <github@juliusappel.com>
2022-02-21 15:16:48 +0100

    Added model 'Appliance'

---
+++ app/models/appliance.rb
@@ -0,0 +1,2 @@
+class Appliance < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_133029) do
+ActiveRecord::Schema.define(version: 2022_02_21_141250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appliances", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id"
+    t.integer "price"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_appliances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
