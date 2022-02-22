3cd545f582950ec94dfa531540831617083aa0f9
Julius Appel <github@juliusappel.com>
2022-02-22 12:13:47 +0100

    Added routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :appliances
+  resources :bookings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
