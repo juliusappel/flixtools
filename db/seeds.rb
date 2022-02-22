eb3d8f2b1af0a2adc12d8992513f2a76cf7c78c6
Miguel Lutz <miguellutz31@gmail.com>
2022-02-22 12:41:54 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, username: Faker::Name.first_name, email: Faker::Internet.email, password: 'shawarma')
+
+  app = Appliance.create!(user: user, title: Faker::Appliance.brand + " " + Faker::Appliance.equipment,
+    description: Faker::Lorem.sentence(word_count: 10),
+    price: rand(1..10), location: Faker::Address.full_address)
+    # Convert price to integer and add € sign in view
+  puts app.title
+end
