3d2e9e24e44f104c07528d22dfc99e614ba07d58
Julius Appel <github@juliusappel.com>
2022-02-22 16:37:11 +0100

    Added lat & log to appliances

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_103617) do
+ActiveRecord::Schema.define(version: 2022_02_22_153309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_02_22_103617) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_appliances_on_user_id"
   end
 
